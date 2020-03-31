c470bba7cc564f658adf7dbddeb8a8069b866388
topino <topino@michele>
2020-03-31 22:25:24 +0200

    bestemmie vaire, file riportandolo alla configurazione originale

--- src/main/java/it/polito/tdp/esempioGit/FXMLController.java
+++ src/main/java/it/polito/tdp/esempioGit/FXMLController.java
@@ -10,31 +10,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
-    
-    @FXML
-    private Label label;
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-    	for(int i = 30; i > 0; i--)
-    		this.time(i);
-        label.setText("Hello World!");
-    }
-    
-    private void time(int i) {
-    	String st = String.valueOf(i);
-		
-    	try {
-			TimeUnit.SECONDS.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		label.setText(st);
-		
+	@FXML
+	private Label label;
+
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		label.setText("Hello World!");
 	}
 
 	@Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
 }
